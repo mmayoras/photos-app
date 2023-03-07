fdcddad67feae7c7dcc763bec3280cb3e9a6ffb8
mmayoras <mkm7874@gmail.com>
2023-03-07 18:29:09 -0500

    Initial commit. Setting up eslint, prettier, typescript basic app, folder structure

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled';
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
+
+const App: React.FC = () => {
+  return (
+    <AppContainer>
+      <AppHeader>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <AppLink
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </AppLink>
+      </AppHeader>
+    </AppContainer>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Global } from '@emotion/react';
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Global
+      styles={{
+        'body': {
+          margin: 0,
+          fontFamily: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+          WebkitFontSmoothing: 'antialiased',
+          MozOsxFontSmoothing: 'grayscale',
+        },
+        'code': {
+          fontFamily: 'source-code-pro, Menlo, Monaco, Consolas, Courier New, monospace',
+        }
+      }}
+    />
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
